112b18c8b784b48af35b8965094af93ee3cecf51
Morza95 <41185928+Morza95@users.noreply.github.com>
2018-07-13 09:37:38 +0000

    Add files via upload

---
+++ Kunden_anlegen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt_Bike___SIT_2018
+{
+    public partial class Kunden_anlegen : Form
+    {
+        public Kunden_anlegen()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Logout_Click(object sender, EventArgs e)
+        {
+            Show(Startseite);
+            Me.hide();
+        }
+    }
+}
